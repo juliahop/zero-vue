5df7a75ba1f84a24a785997fe7232a78dbcb648b
HopMac <hopmac@Air-van-HopMac.home>
2024-02-10 21:19:19 +0100

    Add padding to text in two columb block

--- src/contentblocks/TwoColumnBlock.vue
+++ src/contentblocks/TwoColumnBlock.vue
@@ -2,14 +2,14 @@
   <section class="bg-beige py-lg-8" style="background-color: #EEEAE4;">
     <div class="container">
       <div class="row align-items-center flex-xl-row-reverse">
-        <div class="col-xl-6 py-6">
+        <div class="col-xl-6 px-4 ps-xl-5 py-6">
             <h2 class="mb-4">Hexagon wandelementen. Voor jouw geluksmomentjes van gister, vandaag en morgen.</h2>
             <p class="mb-4">De vorm en uitstraling van de hexagon foto tegel is strak, maar tegelijk speels doordat het losse panelen zijn. Nieuwe hexagon erbij? Bestaande hexagon verwisselen? Het kan allemaal dankzij ons slimme magnetische ophangsysteem. Zo breid je jouw Modulari in de toekomst gemakkelijk uit.</p>
             <p>In 2017 is de hexagon fotocollage door ons bedacht. Toen al razend populair. Nu veelzijdiger dan ooit door nieuwe creatieve mogelijkheden.</p>
           </div>
         <div class="col-xl-6">
           <div>
-            <img src="../assets/images/video-placeholder.jpg" alt="video placeholder">
+            <img class="mw-100" src="../assets/images/video-placeholder.jpg" alt="video placeholder">
           </div>
         </div>        
       </div>
